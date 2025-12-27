ee992750135609ca1a7d2549b9da3c5caf73fe65
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-27 04:54:30 +0200

    delete unused inputs

--- farm-project/src/main.tsx
+++ farm-project/src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import Auth from "./components/Auth/Auth";
 import Dashboard from "./pages/Dashboard";
